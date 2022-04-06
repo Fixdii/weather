bc61cf4cecdb91f5811584ac0964c523987b2b30
Fixdii <walera2001@gmail.com>
2022-04-06 21:17:01 +0300

    add footer and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/toolbar/toolbar.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
